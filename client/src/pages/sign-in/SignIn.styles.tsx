6acf6577a77f389f9b3b8722fc97f221565bd200
weaall <weaall@naver.com>
2024-01-24 22:36:53 +0900

    Add : server side

--- client/src/pages/sign-in/SignIn.styles.tsx
+++ client/src/pages/sign-in/SignIn.styles.tsx
@@ -1,17 +1,25 @@
 import tw from "tailwind-styled-components"
 
-export const Container = tw.div``
-export const ContentsWrap = tw.div`flex flex-col items-center px-[10%]`
+export const Container = tw.div`h-full flex flex-col items-center bg-gradient-to-r from-teal-400 to-teal-100 rounded-t-[32px]`
 
-export const Input = tw.input`w-full h-14 ps-6 pe-6 rounded-[12px] border border-main outline-main my-1`
 
-export const RegBtn = tw.button`bg-gradient-to-r from-teal-400 to-teal-300 
-w-full h-14 font-bold text-white rounded-[12px] my-1`
+export const BannerWrap = tw.div`py-20`
+export const BannerLabel = tw.h1`text-4xl text-white font-bold`
 
-export const PwLabelWrap = tw.div`ml-auto my-3`
-export const PwLabel = tw.button`text-main font-bold`
+export const ContentsBg = tw.div`bg-white/[0.4] w-[90%] rounded-t-3xl h-3`
+export const ContentsWrap = tw.div`bg-white w-full px-[10%] rounded-t-[32px] pt-10 text-center`
 
-export const SocialWrap = tw.div`flex flex-col py-16 text-center`
+export const ContentsLabel = tw.p`text-3xl font-bold`
+export const ContentsText = tw.p`text-base text-gray-400 py-3`
+
+export const Input = tw.input`w-full h-14 ps-6 pe-6 rounded-[12px] border border-main outline-main my-2`
+
+export const RegBtn = tw.button`bg-gradient-to-r from-teal-400 to-teal-200 
+w-full h-14 font-bold text-white rounded-[12px] my-2`
+
+export const PwLabel = tw.button`text-main font-bold my-3`
+
+export const SocialWrap = tw.div`flex flex-col py-8 text-center items-center`
 export const SocialLabel = tw.p`text-gray-400`
 export const SocialButtonWrap = tw.div`flex space-x-8 py-4`
 export const SocialButton = tw.button`w-16 h-16 bg-gray-50 rounded-[12px] p-4`
--- client/src/pages/sign-in/SignIn.tsx
+++ client/src/pages/sign-in/SignIn.tsx
@@ -5,23 +5,26 @@ import * as tw from "./SignIn.styles"
 export default function SignIn() {
     return (
         <tw.Container>
+            <tw.BannerWrap>
+                <tw.BannerLabel>Weaall</tw.BannerLabel>
+            </tw.BannerWrap>
+
+            <tw.ContentsBg />
             <tw.ContentsWrap>
+                <tw.ContentsLabel>Welcome Back</tw.ContentsLabel>
+                <tw.ContentsText>Enter your details below</tw.ContentsText>
                 <tw.Input></tw.Input>
                 <tw.Input></tw.Input>
-                <tw.PwLabelWrap>
-                    <tw.PwLabel>Forgot Password</tw.PwLabel>
-                </tw.PwLabelWrap>
+
                 <tw.RegBtn>Sign in</tw.RegBtn>
+                <tw.PwLabel>Forgot your password?</tw.PwLabel>
 
                 <tw.SocialWrap>
-                    <tw.SocialLabel>Or continue with</tw.SocialLabel>
+                    <tw.SocialLabel>Or sign in with</tw.SocialLabel>
 
                     <tw.SocialButtonWrap>
                         <KaKao />
                         <Naver />
-                        <tw.SocialButton>
-                            <tw.SocialSVG alt="" src={require("../../asset/svg/btn_google.svg").default}></tw.SocialSVG>
-                        </tw.SocialButton>
                     </tw.SocialButtonWrap>
                 </tw.SocialWrap>
             </tw.ContentsWrap>
