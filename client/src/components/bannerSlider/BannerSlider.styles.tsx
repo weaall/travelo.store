b8086a84a193fa740b49761b000712fd60a9620e
weaall <weaall@naver.com>
2024-08-21 20:17:19 +0900

    fix : banner slider layout

--- client/src/components/bannerSlider/BannerSlider.styles.tsx
+++ client/src/components/bannerSlider/BannerSlider.styles.tsx
@@ -1,16 +1,19 @@
 import tw from "tailwind-styled-components"
 
-export const Container = tw.div`rounded-xl border flex flex-col items-center`
+export const Container = tw.div`relative flex flex-col items-center overflow-hidden w-full py-10 px-5`
 
-export const ImgContainer = tw.div`relative w-full h-[200px] flex items-center justify-center overflow-hidden`;
-export const Img = tw.img`object-cover w-[335px] h-[130px]`
-
-export const ImgButton = tw.button`absolute top-1/2 transform -translate-y-1/2 bg-gray-500 bg-opacity-40 text-white w-10 h-10 text-xl rounded-2xl px-2`;
+export const ImgContainer = tw.div`relative w-full flex items-center justify-center overflow-hidden group`;
+export const ImgListWrap = tw.div`flex transition-transform duration-500 ease-in-out`
+export const ImgWrap = tw.div`w-[50%] px-1 h-[140px] flex-shrink-0 mobile:w-[100%] cursor-pointer`
+export const ImgButton = tw.button`absolute top-1/2 transform -translate-y-1/2 bg-gray-700 bg-opacity-40 hidden
+text-white w-10 h-10 text-xl rounded-full p-2 z-10 hover:bg-opacity-60
+group-hover:block mobile:group-hover:hidden `;
 
 export const UnRegWrap = tw.div`flex h-full justify-center items-center text-3xl font-bold`
 
 export const Svg = tw.img`w-full h-full `
 
-
-export const IndexWrap = tw.div`flex`
-export const Index = tw.p`text-xs p-3`
+export const IndexWrap = tw.div`flex justify-center items-center`
+export const ToggleBtn = tw.button`w-4 mx-3`
+export const Index = tw.div`flex text-5xl font-medium my-3 px-3 border-l border-gray-300`
+export const IndexSpan = tw.span`pl-1 text-gray-500`
--- client/src/components/bannerSlider/BannerSlider.tsx
+++ client/src/components/bannerSlider/BannerSlider.tsx
@@ -1,18 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import * as tw from "./BannerSlider.styles";
 import ImgLoader from "../../utils/imgLoader";
+import { useNavigate } from "react-router-dom";
 
 export default function BannerSlider() {
+    const navigate = useNavigate();
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(true);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 640); 
 
     const images = [
-        { url: "https://cdn.pixabay.com/photo/2023/08/07/19/47/water-lily-8175845_1280.jpg" },
-        { url: "https://cdn.pixabay.com/photo/2023/08/05/08/15/ship-8170663_1280.jpg" },
-        { url: "https://cdn.pixabay.com/photo/2017/07/06/19/57/sky-2479213_1280.jpg" },
-        { url: "https://cdn.pixabay.com/photo/2023/08/07/19/47/water-lily-8175845_1280.jpg" },
-        { url: "https://cdn.pixabay.com/photo/2023/08/05/08/15/ship-8170663_1280.jpg" },
-        { url: "https://cdn.pixabay.com/photo/2017/07/06/19/57/sky-2479213_1280.jpg" },
+        { url: "https://cdn.pixabay.com/photo/2023/08/07/19/47/water-lily-8175845_1280.jpg", href: "/" },
+        { url: "https://cdn.pixabay.com/photo/2023/08/05/08/15/ship-8170663_1280.jpg", href: "/" },
+        { url: "https://cdn.pixabay.com/photo/2017/07/06/19/57/sky-2479213_1280.jpg" , href: "/" },
+        { url: "https://cdn.pixabay.com/photo/2023/08/07/19/47/water-lily-8175845_1280.jpg" , href: "/" },
+        { url: "https://cdn.pixabay.com/photo/2023/08/05/08/15/ship-8170663_1280.jpg" , href: "/" },
+        { url: "https://cdn.pixabay.com/photo/2017/07/06/19/57/sky-2479213_1280.jpg", href: "/" },
     ];
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 640);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout;
+        if (isPlaying) {
+            interval = setInterval(() => {
+                nextSlide();
+            }, 5000);
+        }
+
+        return () => {
+            if (interval) {
+                clearInterval(interval);
+            }
+        };
+    }, [isPlaying, currentIndex]);
 
     const prevSlide = () => {
         const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
@@ -24,33 +52,57 @@ export default function BannerSlider() {
         setCurrentIndex(newIndex);
     };
 
+    const togglePlay = useCallback(() => {
+        setIsPlaying(prev => !prev);
+    }, []);
+
+    const containerStyle = {
+        transform: `translateX(-${currentIndex * (isMobile ? 100 : 50)}%)`,
+        width: `${(images.length + 1) * 100}%`
+    };
+
     if (!images || images.length === 0) {
         return <tw.UnRegWrap>미등록</tw.UnRegWrap>;
     }
 
     return (
-        <div className="relative overflow-hidden w-full max-w-4xl mx-auto">
-            <div className="flex items-center">
-                <tw.ImgButton onClick={prevSlide} className="absolute left-0 z-10 p-2 bg-gray-700 text-white rounded-full hover:bg-gray-500">
+        <tw.Container>
+            <tw.ImgContainer>
+                <tw.ImgButton onClick={prevSlide} className="left-0">
                     <tw.Svg src={require("../../assets/svg/left_icon.svg").default} alt="Left" />
                 </tw.ImgButton>
-                <div
-                    className="flex transition-transform duration-500 ease-in-out"
-                    style={{ transform: `translateX(-${(currentIndex) * 50}%)`, width: `${images.length * 100}%` }}
-                >
+                <tw.ImgListWrap style={containerStyle}>
                     {images.map((image, index) => (
-                        <div key={index} className="w-[50%] h-[130px] flex-shrink-0">
-                            <ImgLoader imageUrl={image.url} altText={`Slide ${index}`} />
-                        </div>
+                        <tw.ImgWrap onClick={()=>navigate(`${image.href}`)}>
+                            <ImgLoader imageUrl={image.url} altText={`Slide ${index}`} rounded="2xl"/>
+                        </tw.ImgWrap>
                     ))}
-                </div>
-                <tw.ImgButton onClick={nextSlide} className="absolute right-0 z-10 p-2 bg-gray-700 text-white rounded-full hover:bg-gray-500">
+                    <tw.ImgWrap>
+                        <ImgLoader imageUrl={images[0].url} altText={`Slide 0`} rounded="2xl"/>
+                    </tw.ImgWrap>
+                </tw.ImgListWrap>
+                <tw.ImgButton onClick={nextSlide} className="right-0">
                     <tw.Svg src={require("../../assets/svg/right_icon.svg").default} alt="Right" />
                 </tw.ImgButton>
-            </div>
-            <div className="text-center mt-2">
-                {currentIndex + 1} / {images.length}
-            </div>
-        </div>
+            </tw.ImgContainer>
+            <tw.IndexWrap>
+                <tw.ToggleBtn onClick={togglePlay}>
+                    {isPlaying ? (
+                        <tw.Svg src={require("../../assets/svg/pause_icon.svg").default} alt="Pause" />
+                    ) : (
+                        <tw.Svg src={require("../../assets/svg/play_icon.svg").default} alt="Play" />
+                    )}
+                </tw.ToggleBtn>
+                <tw.Index>
+                    <tw.ToggleBtn onClick={prevSlide}>
+                        <tw.Svg src={require("../../assets/svg_black/left_icon.svg").default} alt="Pause" />
+                    </tw.ToggleBtn>
+                        {currentIndex + 1} /<tw.IndexSpan>{images.length}</tw.IndexSpan>
+                    <tw.ToggleBtn onClick={nextSlide}>
+                        <tw.Svg src={require("../../assets/svg_black/right_icon.svg").default} alt="Pause" />
+                    </tw.ToggleBtn>
+                </tw.Index>
+            </tw.IndexWrap>
+        </tw.Container>
     );
 }
